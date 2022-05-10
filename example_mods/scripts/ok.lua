a3a9d1f5d695aa7572b2bde64fbebf3bee5d13e7
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-05-10 08:18:03 -0400

    i add new cover!!!!!!!!!!!!!!!

---
+++ example_mods/scripts/ok.lua
@@ -0,0 +1,6 @@
+function onUpdatePost()
+	setProperty("iconP1.scale.y", (getProperty("iconP1.scale.y") - 1) / -0.8 + 1)
+	setProperty("iconP2.scale.y", (getProperty("iconP2.scale.y") - 1) / -0.8 + 1)
+	setProperty("iconP1.y", 500 + (getProperty("iconP1.scale.y") * 75))
+	setProperty("iconP2.y", 500 + (getProperty("iconP2.scale.y") * 75))
+end
