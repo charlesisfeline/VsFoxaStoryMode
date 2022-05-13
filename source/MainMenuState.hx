57802c45219014a3806f6c66ce969f2343b3c1f9
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-05-12 20:23:31 -0400

    imma just make this a comment for some moments

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -127,7 +127,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.updateHitbox();
 			
 			
-			switch (i) {
+			/*switch (i) {
 					case 0: 
 						menuItem.y = 34.95;
 						menuItem.x = 299.4;
@@ -142,7 +142,7 @@ class MainMenuState extends MusicBeatState
 					case 3:
 						menuItem.x = 712.9;
 						menuItem.y = 594.75;
-			}
+			}*/
 			
 			
 			if(FlxG.save.data.antialiasing)
