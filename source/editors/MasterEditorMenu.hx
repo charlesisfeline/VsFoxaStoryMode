c20e947b12fc6615308a866e434b40a2afa3aefe
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-05-13 21:50:01 -0400

    what

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -77,7 +77,7 @@ class MasterEditorMenu extends MusicBeatState
 		var found:Int = directories.indexOf(Paths.currentModDirectory);
 		if(found > -1) curDirectory = found;
 		changeDirectory();
-		#end
+
 		changeSelection();
 
 		FlxG.mouse.visible = false;
